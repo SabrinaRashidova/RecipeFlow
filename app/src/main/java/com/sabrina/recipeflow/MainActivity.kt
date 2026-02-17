67cfd658f9797e6d709a9d7102c7a630ac81d1ce
SabrinaRashidova <rashidovasabrina2@gmail.com>
2026-02-17 15:24:19 +0500

    feat(ui): added Material 3 Bottom Navigation

--- app/src/main/java/com/sabrina/recipeflow/MainActivity.kt
+++ app/src/main/java/com/sabrina/recipeflow/MainActivity.kt
@@ -4,9 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.sabrina.recipeflow.ui.navigation.Screen
 import com.sabrina.recipeflow.ui.navigation.SetupNavGraph
 import com.sabrina.recipeflow.ui.screens.RecipeScreen
 import com.sabrina.recipeflow.ui.theme.RecipeFlowTheme
@@ -19,11 +36,57 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             RecipeFlowTheme {
-               val navController = rememberNavController()
-                Surface(color = MaterialTheme.colorScheme.background) {
-                    SetupNavGraph(navController = navController)
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainScreen()
                 }
             }
         }
+    }
+}
+
+
+@Composable
+fun MainScreen() {
+    val navController = rememberNavController()
+
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
+    Scaffold(
+        bottomBar = {
+            NavigationBar(containerColor = MaterialTheme.colorScheme.surfaceVariant, tonalElevation = 8.dp) {
+                NavigationBarItem(
+                    selected = currentRoute == Screen.RecipeSearch.route,
+                    onClick = {
+                        navController.navigate(Screen.RecipeSearch.route){
+                            popUpTo(navController.graph.startDestinationId)
+                            launchSingleTop = true
+                        }
+                    },
+                    icon = { Icon(Icons.Default.Search,contentDescription = "Search") },
+                    label = { Text("Search") }
+                )
+
+                NavigationBarItem(
+                    selected = currentRoute == Screen.Favorites.route,
+                    onClick = {
+                        navController.navigate(Screen.Favorites.route){
+                            popUpTo(navController.graph.startDestinationId)
+                            launchSingleTop = true
+                        }
+                    },
+                    icon = { Icon(Icons.Default.Favorite, contentDescription = "Favorites") },
+                    label = { Text("Favorites") }
+                )
+            }
+        }
+    ) {innerPadding ->
+        Box(modifier = Modifier.padding(innerPadding)) {
+            SetupNavGraph(navController = navController)
+        }
+
     }
 }
